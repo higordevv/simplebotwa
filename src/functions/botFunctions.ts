9467824b5db35d5b7ae6efa04e6220d53a5312bc
isaias-silva <zackblackmangold@gmail.com>
2022-08-21 13:09:09 -0300

    isowner

--- src/functions/botFunctions.ts
+++ src/functions/botFunctions.ts
@@ -1,11 +1,16 @@
 import { proto} from "@adiwajshing/baileys";
+import { data } from "../bot_config/config";
 import { Ibot } from "../interfaces/Ibot";
 
 export const botFunctions=(webMessage:proto.IWebMessageInfo,socket:any):Ibot=>{
     const { remoteJid, participant } = webMessage.key
     
     const botInfo = socket.user
-    const isOwner =async()=>{
+    const isOwner =async(number:string)=>{
+        let numberFormated=number.split(`@`)[0]      
+        if(numberFormated==data.owner){
+            return true
+        }
         return false
     }
     const sendText = async (txt: string) => {
