499bcd2cf0eb99f04fa9e15bea4bedb2aea7c173
higordevv <pedrohigor2004@gmail.com>
2022-09-16 18:03:45 -0300

    Changes to be committed:     - Voltei pra minimizar os conflitos 	renamed:    src/model/cliente_model/cliente.ts -> src/model/cliente.ts 	renamed:    src/model/cliente_model/clienteModel.ts -> src/model/clienteModel.ts 	deleted:    src/model/fluxo_model/fluxo.ts 	deleted:    src/model/fluxo_model/fluxoModel.ts

--- src/model/fluxo_model/fluxo.ts
+++
@@ -1,4 +0,0 @@
-import mongoose from "../db";
-import FluxoSchema from "./fluxoModel";
-
-//CRUD FLUXO
--- src/model/fluxo_model/fluxoModel.ts
+++
@@ -1,4 +0,0 @@
-import mongoose from "../db";
-
-const FluxoSchema = new mongoose.Schema({}, { collection: "fluxo" });
-export default FluxoSchema;
