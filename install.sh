4ecb4f13ddb53555bbac6e71afe58ac5c4c45e8b
higordevv <pedrohigor2004@gmail.com>
2022-09-16 16:30:12 -0300

    Changes to be committed: 	modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,19 @@
-# Refazer Jaja
+!/bin/bash
+
+
+echo "INSTALA O CURL"
+sudo apt install curl -y 
+
+echo "BAIXA E INSTALA O NODE 16+"
+curl -sL https://deb.nodesource.com/setup_16.x | sudo bash - 
+sudo apt install nodejs -y 
+
+npm install 
+
+npm run build 
+
+echo "rm -rf install.sh && start.sh" >> start.sh
+
+chmod 777 start.sh
+
+./start.sh
